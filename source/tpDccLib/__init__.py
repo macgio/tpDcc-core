932e672e84c84969bfeb6b19c35829c435254d45
Tomi Poveda <tpoveda@cgart3d.com>
2019-04-27 04:51:43 +0200

    Improve import/reload workflow

--- source/tpDccLib/__init__.py
+++ source/tpDccLib/__init__.py
@@ -10,16 +10,15 @@ from __future__ import print_function, division, absolute_import
 main = __import__('__main__')
 
 import os
-import types
-import pkgutil
-import importlib
-from collections import OrderedDict
+import inspect
 
-from tpPyUtils import logger as logger_utils
+from tpPyUtils import importer
+from tpQtLib.core import resource as resource_utils
 
 # =================================================================================
 
 logger = None
+resource = None
 Dcc = None
 
 # =================================================================================
@@ -34,171 +33,69 @@ class Dccs(object):
 # =================================================================================
 
 
-class tpDccLib(object):
+class tpDccLibResource(resource_utils.Resource, object):
+    RESOURCES_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
 
-    loaded_modules = OrderedDict()
-    reload_modules = list()
 
-    @classmethod
-    def initialize(cls, do_reload=False):
+class tpDccLib(importer.Importer, object):
+    def __init__(self):
+        super(tpDccLib, self).__init__(module_name='tpDccLib')
 
-        import tpDccLib
-
-        cls.create_logger()
-        cls.init_dcc(do_reload=do_reload)
-        cls.import_modules(os.path.join(tpDccLib.__path__[0], 'core'), only_packages=True, order=['tpDccLib.widgets'])
-
-        if do_reload:
-            cls.reload_all()
-
-    @staticmethod
-    def create_logger():
+    def get_module_path(self):
         """
-        Creates and initializes tpDccLib logger
+        Returns path where tpDccLib module is stored
+        :return: str
         """
-
-        global logger
-        logger = logger_utils.Logger(name='tpDccLib', level=logger_utils.LoggerLevel.WARNING).logger
-        logger.debug('Initializing tpDccLib logger ...')
-        return logger
-
-    @classmethod
-    def init_dcc(cls, do_reload=False):
-        """
-        Checks DCC we are working on an initializes proper variables
-        """
-
-        if 'cmds' in main.__dict__:
-            import tpMayaLib
-            tpMayaLib.init(do_reload=do_reload)
-        elif 'MaxPlus' in main.__dict__:
-            import tpMaxLib
-            tpMaxLib.init(do_reload=do_reload)
-        elif 'hou' in main.__dict__:
-            raise NotImplementedError('Houdini is not a supported DCC yet!')
-        elif 'nuke' in main.__dict__:
-            raise NotImplementedError('Nuke is not a supported DCC yet!')
-        else:
-            raise NotImplementedError('Current DCC is not supported yet!')
-
-    @staticmethod
-    def _explore_package(module_name, only_packages=False):
-        """
-        Load module iteratively
-        :param module_name: str, name of the module
-        :return: list<str>, list<str>, list of loaded module names and list of loaded module paths
-        """
-
-        import tpDccLib
-
-        module_names = list()
-        module_paths = list()
-
-        def foo(name, only_packages):
-            for importer, m_name, is_pkg in pkgutil.iter_modules([name]):
-                mod_path = name + "//" + m_name
-                mod_name = 'tpDccLib.' + os.path.relpath(mod_path, tpDccLib.__path__[0]).replace('\\', '.')
-                if only_packages:
-                    if is_pkg:
-                        module_paths.append(mod_path)
-                        module_names.append(mod_name)
-                else:
-                    module_paths.append(mod_path)
-                    module_names.append(mod_name)
-                # foo(mod_path, only_packages)
-
-        foo(module_name, only_packages)
-
-        return module_names, module_paths
-
-    @staticmethod
-    def _import_module(package_name):
-
-        import tpDccLib
 
         try:
-            mod = importlib.import_module(package_name)
-            tpDccLib.logger.debug('Imported: {}'.format(package_name))
-            if mod and isinstance(mod, types.ModuleType):
-                return mod
-            return None
-        except (ImportError, AttributeError) as e:
-            tpDccLib.logger.error('FAILED IMPORT: {} -> {}'.format(package_name, str(e)))
-            pass
+            mod_dir = os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename)
+        except Exception:
+            try:
+                mod_dir = os.path.dirname(__file__)
+            except Exception:
+                try:
+                    import tpDccLib
+                    mod_dir = tpDccLib.__path__[0]
+                except Exception:
+                    return None
 
-    @classmethod
-    def import_modules(cls, module_name, only_packages=False, order=[]):
-        names, paths = cls._explore_package(module_name=module_name, only_packages=only_packages)
-        ordered_names = list()
-        ordered_paths = list()
-        temp_index = 0
-        i = -1
-        for o in order:
-            for n, p in zip(names, paths):
-                if str(n) == str(o):
-                    i += 1
-                    temp_index = i
-                    ordered_names.append(n)
-                    ordered_paths.append(p)
-                elif n.endswith(o):
-                    ordered_names.insert(temp_index + 1, n)
-                    ordered_paths.insert(temp_index + 1, n)
-                    temp_index += 1
-                elif str(o) in str(n):
-                    ordered_names.append(n)
-                    ordered_paths.append(p)
-
-        ordered_names.extend(names)
-        ordered_paths.extend(paths)
-
-        names_set = set()
-        paths_set = set()
-        module_names = [x for x in ordered_names if not (x in names_set or names_set.add(x))]
-        module_paths = [x for x in ordered_paths if not (x in paths_set or paths_set.add(x))]
-
-        reloaded_names = list()
-        reloaded_paths = list()
-        for n, p in zip(names, paths):
-            reloaded_names.append(n)
-            reloaded_paths.append(p)
-
-        for name, _ in zip(module_names, module_paths):
-            if name not in cls.loaded_modules.keys():
-                mod = cls._import_module(name)
-                if mod:
-                    if isinstance(mod, types.ModuleType):
-                        cls.loaded_modules[mod.__name__] = [os.path.dirname(mod.__file__), mod]
-                        cls.reload_modules.append(mod)
-
-        for name, path in zip(module_names, module_paths):
-            order = list()
-            if name in cls.loaded_modules.keys():
-                mod = cls.loaded_modules[name][1]
-                if hasattr(mod, 'order'):
-                    order = mod.order
-            cls.import_modules(module_name=path, only_packages=False, order=order)
-
-    @classmethod
-    def reload_all(cls):
-        """
-        Reload all current loaded modules
-        """
-
-        import tpDccLib
-
-        tpDccLib.logger.debug(' =========> Reloading tpDccLib ...')
-
-        for mod in cls.reload_modules:
-            if not hasattr(mod, 'no_reload'):
-                tpDccLib.logger.debug('RELOADING: {}'.format(mod.__name__))
-                reload(mod)
-            else:
-                tpDccLib.logger.debug('AVOIDING RELOAD OF {}'.format(mod))
-        tpDccLib.logger.debug(' =========> tpDccLib reloaded successfully!')
+        return mod_dir
 
 
 def init(do_reload=False):
-    tpDccLib.initialize(do_reload=do_reload)
+    """
+    Initializes module
+    :param do_reload: bool, Whether to reload modules or not
+    """
+
+    dcclib_importer = importer.init_importer(importer_class=tpDccLib, do_reload=do_reload)
+    init_dcc(do_reload=do_reload)
+
+    global logger
+    global resource
+    logger = dcclib_importer.logger
+    resource = tpDccLibResource
+
+    dcclib_importer.import_modules()
+    dcclib_importer.import_packages(only_packages=True)
+
+def init_dcc(do_reload=False):
+    """
+    Checks DCC we are working on an initializes proper variables
+    """
+
+    if 'cmds' in main.__dict__:
+        import tpMayaLib
+        tpMayaLib.init(do_reload=do_reload)
+    elif 'MaxPlus' in main.__dict__:
+        import tpMaxLib
+        tpMaxLib.init(do_reload=do_reload)
+    elif 'hou' in main.__dict__:
+        raise NotImplementedError('Houdini is not a supported DCC yet!')
+    elif 'nuke' in main.__dict__:
+        raise NotImplementedError('Nuke is not a supported DCC yet!')
+    else:
+        raise NotImplementedError('Current DCC is not supported yet!')
 
 
 def is_nuke():
