94f5955262c401feaecea67e43b81389a980f82c
Tomi Poveda <tpovedatd@gmail.com>
2020-01-09 01:47:53 +0100

    Added generic functions to check whether a given name is in the center/right/side from the center of the world

--- source/tpDccLib/abstract/dcc.py
+++ source/tpDccLib/abstract/dcc.py
@@ -1788,6 +1788,57 @@ class AbstractDCC(object):
     # ================================================================================================================
 
     @staticmethod
+    def name_is_center(side, patterns=None):
+        """
+        Returns whether given side is a valid center side or not
+        :param side: str
+        :param patterns: list<str>
+        :return: bool
+        """
+
+        if not patterns:
+            patterns = ['C', 'c', 'Center', 'ct', 'center', 'middle', 'm']
+
+        if str(side) in patterns:
+            return True
+
+        return False
+
+    @staticmethod
+    def name_is_left(side, patterns=None):
+        """
+        Returns whether given side is a valid left side or not
+        :param side: str
+        :param patterns: list<str>
+        :return: bool
+        """
+
+        if not patterns:
+            patterns = ['L', 'l', 'Left', 'left', 'lf']
+
+        if str(side) in patterns:
+            return True
+
+        return False
+
+    @staticmethod
+    def name_is_right(side, patterns=None):
+        """
+        Returns whether given side is a valid right side or not
+        :param side: str
+        :param patterns: list<str>
+        :return: bool
+        """
+
+        if not patterns:
+            patterns = ['R', 'r', 'Right', 'right', 'rt']
+
+        if str(side) in patterns:
+            return True
+
+        return False
+
+    @staticmethod
     def get_dockable_window_class():
         """
         Returns class that should be used to instance an ew dockable DCC window
