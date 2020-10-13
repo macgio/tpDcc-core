66fe7dd9a4b2439de5a9d56f0ff190f809b63138
Tomas Poveda <tpovedatd@gmail.com>
2020-10-13 03:46:41 +0200

    Added DCC command implementation

--- tpDcc/core/exceptions.py
+++ tpDcc/core/exceptions.py
@@ -18,3 +18,13 @@ class NoMatchFoundError(DccError):
 
 class NoObjectFoundError(DccError):
     pass
+
+
+class CommandCancel(DccError):
+    def __init__(self, message, errors=None):
+        super(CommandCancel, self).__init__(message)
+        self._errors = errors
+
+    @property
+    def errors(self):
+        return self._errors
